d6f5a0f0a5e77a396f8a6cb7949c5d829ec6b820
Mattias Villani <mattias.villani@gmail.com>
2026-09-07 20:51:18 +0200

    first push of vaxjo

--- logo.js
+++ logo.js
@@ -1,9 +1,9 @@
 <script>
 const logo = document.createElement('img');
 logo.src = 'figs/compBayesLogo_mvcolors.png';
-logo.style.cssText = 'position:fixed; bottom: 50px; left:50%; transform:translateX(-50%); width:600px; z-index:9998; pointer-events:none; display:none;';
+logo.style.cssText = 'position:absolute; bottom: 50px; left:50%; transform:translateX(-50%); width:600px; z-index:9998; pointer-events:none; display:none;';
 logo.classList.add('no-print');
-document.body.appendChild(logo);
+document.querySelector('.reveal .slides').appendChild(logo);
 
 Reveal.on('ready', event => {
   if (event.indexh === 0) logo.style.display = 'block';
